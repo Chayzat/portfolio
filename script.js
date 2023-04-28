fbc61a7eb7308e003d15fdb7cefbb0a643b5fbcd
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-04-28 19:46:45 +0300

    feat: update design

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
-let container = document.querySelector(".main-section");
-let section = container.querySelectorAll(".section");
+const info = document.querySelector('#info')
+const links = document.querySelectorAll('.nav-item')
+const infoSection = document.querySelector('.info-section')
 
-var swiper = new Swiper('.mySwiper', {
-  spaceBetween: 30,
-  pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-  },
-  navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-  }
+const linksArr = Array.from(links)
+
+if (links.length) {
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      links.forEach(link => link.classList.remove('active'))
+      link.classList.add('active');
+    });
+  });
+}
+
+info.addEventListener('click', () => {
+  infoSection.classList.toggle('show')
 })
-
-gsap.registerPlugin(ScrollTrigger);
-
-let tl = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: container,
-      scrub: 1,
-      pin: true,
-      start: "top top",
-      end: "+=3300",
-    },
-  })
-  .to(container, {
-    x: () =>
-      -(container.scrollWidth - document.documentElement.clientWidth - 20) +
-      "px",
-    ease: "none",
-    duration: 1,
-  })
-
-  ScrollTrigger.saveStyles(".about-section, .project-section, .main-section");
-
-
-
