e9e6e2fdc6d0e5f888c2d6cbdd3f33b86ac627a3
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-04-29 18:47:11 +0300

    update: change design

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const info = document.querySelector('#info')
-const links = document.querySelectorAll('.nav-item')
 const infoSection = document.querySelector('.info-section')
 
-const linksArr = Array.from(links)
-
-if (links.length) {
-  links.forEach(link => {
-    link.addEventListener('click', () => {
-      links.forEach(link => link.classList.remove('active'))
-      link.classList.add('active');
-    });
-  });
-}
-
 info.addEventListener('click', () => {
+  info.classList.toggle('active')
   infoSection.classList.toggle('show')
 })
+
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  loop: true,
+  mousewheel: true,
+  keyboard: {
+    enabled: true,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
