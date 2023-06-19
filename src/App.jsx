37fb22aca2d76e02449fd2ff8d76c9580cd33a20
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-06-19 18:40:30 +0300

    feat: upgrade design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route to="/" element={<Layout />}>
           <Route index element={<Home />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,53 @@
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      <h1>CHAYZAT</h1>
+    <div className="container">
+      <div className="line">
+        <p className="text">
+          <span>ocm{">"}</span> Hi, {"I'm"} Chayzat!
+        </p>
+      </div>
+      <div className="line">
+        <p className="text">
+          <span>ocm{">"}</span> I code responsive websites.
+        </p>
+      </div>
+      <div className="line">
+        <p className="text">
+          <span>ocm{">"}</span> I am currently accepting job offers.
+        </p>
+      </div>
+      <div className="line">
+        <p className="text">
+          <span>ocm{">"}</span> You can contact me at:
+        </p>
+      </div>
+      <div className="line">
+        <p className="text">
+          <span>{">"} </span>
+          <Link to="/">chayzat.oorzhak.2001@mail.ru</Link>
+        </p>
+      </div>
+      <div className="line">
+        <p className="text">
+          <span>{">"} </span>
+          <Link to="/">github.com/Chayzat</Link>
+        </p>
+      </div>
+      <div className="line">
+        <p className="text">
+          <span>ocm{">"}</span> Have a good day! 🙃
+        </p>
+      </div>
+      <div className="line">
+        {" "}
+        <p className="text underscore">
+          <span>ocm{">"} </span>{" "}
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
